c03e7de87620302b92a3b70aa13ed45a9f96774e
fisherpavel <fisherpavel@bk.ru>
2020-06-11 08:22:05 +0700

    Add project

---
+++ api/routes/user.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const checkAuth = require('../middleware/check-auth')
+const User = require('../models/user')
+
+router.post('/signup', async (req, res) => {
+   const user = await User.find({email: req.body.email})
+    if(user.length >= 1){
+        return res.status(409).json({
+            message: 'Mail exists'
+        })
+    } else {
+        bcrypt.hash(req.body.password, 10, async (err, hash) => {
+            if(err){
+                return res.status(500).json({
+                    error: err
+                })
+            } else {
+                try{
+                    const user = await new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    })
+                  const result =  await user.save()
+                  console.log(result)
+                    res.status(201).json({
+                        message: 'User created'
+                    })
+                } catch(err){
+                    console.log(err)
+                    res.status(500).json({
+                        error: err
+                    })
+                }
+              
+            }
+        })
+    }
+ 
+})
+
+router.post('/login', async (req, res) => {
+    try{
+       const user = await User.find({email: req.body.email})
+        if(user.length < 1){
+            return res.status(401).json({
+                message: `Auth failed`
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err){
+                return res.status(401).json({
+                    message: `Auth failed`
+                })
+            }
+            if(result){
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, 
+                process.env.JWT_KEY, 
+                {
+                    expiresIn: "1h"
+                }
+                )
+               return res.status(200).json({
+                    message: 'Auth successful',
+                    token
+                })
+            }
+            res.status(401).json({
+                message: `Auth failed`
+            })
+        })
+
+    } catch(err){
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    }
+})
+
+
+router.delete('/:userId', checkAuth, async (req, res) => {
+    try {
+       const result = await User.remove({_id: req.params.userId})
+       res.status(200).json({
+           message: 'User deleted'
+       })
+    } catch(err){
+        console.log(err)
+                    res.status(500).json({
+                        error: err
+                    })
+    }
+} )
+
+
+module.exports = router
---
+++ app.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const productRoutes = require('./api/routes/products')
+const ordersRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/user')
+
+mongoose.connect(`mongodb+srv://pavel:`+process.env.MONGO_ATLAS_PW+`@cluster0-hid6k.mongodb.net/restapi?retryWrites=true&w=majority`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+app.use(morgan('dev'))
+app.use('/uploads' ,express.static('uploads'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
+
+app.use('/products', productRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/user', userRoutes)
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app
+
+
